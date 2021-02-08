27925c99a77a2608303a749a62c8e3f0ec2fd105
Yura Levchenko <yuralev240297@gmail.com>
2021-02-08 18:26:30 +0200

    config container DI

--- CustomWishlist.WebUI/App_Start/NinjectWebCommon.cs
+++ CustomWishlist.WebUI/App_Start/NinjectWebCommon.cs
@@ -61,6 +61,7 @@ namespace CustomWishlist.WebUI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            System.Web.Mvc.DependencyResolver.SetResolver(new CustomWishlist.WebUI.Infrastructure.NinjectDependencyRecolver(kernel));
         }        
     }
 }
---
+++ CustomWishlist.WebUI/Infrastructure/NinjectDependencyRecolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Ninject;
+
+namespace CustomWishlist.WebUI.Infrastructure
+{
+    public class NinjectDependencyRecolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyRecolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            // привязки
+        }
+    }
+}
