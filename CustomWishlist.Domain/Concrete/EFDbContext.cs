ee90c765d1583646659226510d6ba459a032dea4
Yura Levchenko <yuralev240297@gmail.com>
2021-02-11 17:45:08 +0200

    Defining the database schema, adding data to the database, connecting the Entity Framework

---
+++ CustomWishlist.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,10 @@
+﻿using CustomWishlist.Domain.Entities;
+using System.Data.Entity;
+
+namespace CustomWishlist.Domain.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
