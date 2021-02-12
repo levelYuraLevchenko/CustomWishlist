a4eb8bfc13f226f82cf1e16225fc86e390aed16b
Yura Levchenko <yuralev240297@gmail.com>
2021-02-12 17:01:59 +0200

    Added pagination support

--- CustomWishlist.UnitTests/UnitTest1.cs
+++ CustomWishlist.UnitTests/UnitTest1.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using CustomWishlist.Domain.Abstract;
+using CustomWishlist.Domain.Entities;
+using CustomWishlist.WebUI.Controllers;
 
 namespace CustomWishlist.UnitTests
 {
@@ -7,8 +12,22 @@ namespace CustomWishlist.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            //
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            //controller.
         }
     }
 }
